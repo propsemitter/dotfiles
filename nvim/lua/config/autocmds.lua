518b34ca4b29696feb23e60fda74a0186fa6a6f8
adubov <dv.idaldu@gmail.com>
2026-03-12 10:38:18 +0600

    feat: add autocmds

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
+  pattern = "*.vue",
+  callback = function()
+    local buf_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    if #buf_lines == 1 and buf_lines[1] == "" then
+      local template = vim.fn.expand("~/.config/nvim/templates/vue.vue")
+      local lines = vim.fn.readfile(template)
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+    end
+  end,
+})
