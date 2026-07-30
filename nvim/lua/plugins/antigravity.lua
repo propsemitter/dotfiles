40b1d8ee656ed88f4eb23923ecf08ff33288048d
adubov <dv.idaldu@gmail.com>
2026-07-30 13:09:27 +0600

    feat(nvim): add keymaps <leader>ag to toggle Antigravity

--- nvim/lua/plugins/antigravity.lua
+++ nvim/lua/plugins/antigravity.lua
@@ -8,6 +8,11 @@ return {
     opts = {
       command = "agy",
     },
+    keys = {
+      { "<leader>ag", "<cmd>AntigravityToggle<cr>", desc = "Toggle Antigravity" },
+      { "<leader>ac", "<cmd>AntigravityConnect<cr>", desc = "Antigravity Connect" },
+    },
+
 
   },
 }
