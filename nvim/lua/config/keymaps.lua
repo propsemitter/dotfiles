8c00748a84ca6a4c3918f3a4c23f322e6355a87f
adubov <dv.idaldu@gmail.com>
2026-07-30 15:16:01 +0600

    fix(nvim): use vim.fn.fnamemodify for guaranteed relative path calculation

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -25,23 +25,25 @@ map("v", "<Right>", "<cmd>echom " .. msg .. "<CR>", { noremap = true })
 
 -- Copy file path keymaps
 map("n", "<leader>fr", function()
-  local path = vim.fn.expand("%")
-  if path ~= "" then
-    vim.fn.setreg("+", path)
-    vim.notify("Copied relative path: " .. path, vim.log.levels.INFO)
+  local abs_path = vim.api.nvim_buf_get_name(0)
+  if abs_path ~= "" then
+    local rel_path = vim.fn.fnamemodify(abs_path, ":.")
+    vim.fn.setreg("+", rel_path)
+    vim.notify("Copied relative path: " .. rel_path, vim.log.levels.INFO)
   else
     vim.notify("No file name for current buffer", vim.log.levels.WARN)
   end
 end, { desc = "Copy relative path" })
 
 map("n", "<leader>fY", function()
-  local path = vim.fn.expand("%:p")
-  if path ~= "" then
-    vim.fn.setreg("+", path)
-    vim.notify("Copied absolute path: " .. path, vim.log.levels.INFO)
+  local abs_path = vim.api.nvim_buf_get_name(0)
+  if abs_path ~= "" then
+    vim.fn.setreg("+", abs_path)
+    vim.notify("Copied absolute path: " .. abs_path, vim.log.levels.INFO)
   else
     vim.notify("No file name for current buffer", vim.log.levels.WARN)
   end
 end, { desc = "Copy absolute path" })
+
 
 
