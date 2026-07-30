d1a60eaaa9717f9ad2ae8cbd2ffca1cfa9510592
adubov <dv.idaldu@gmail.com>
2026-07-30 15:21:20 +0600

    feat(nvim): add Ctrl+g shortcut to toggle Antigravity float directly from terminal mode

--- nvim/lua/plugins/antigravity.lua
+++ nvim/lua/plugins/antigravity.lua
@@ -14,8 +14,10 @@ return {
     },
     keys = {
       { "<leader>ag", "<cmd>lua require('antigravity').toggle()<cr>", desc = "Toggle Antigravity Float" },
+      { "<c-g>", "<cmd>lua require('antigravity').toggle()<cr>", mode = { "n", "t" }, desc = "Toggle Antigravity Terminal" },
       { "<leader>as", "<cmd>lua require('antigravity').ask_selection()<cr>", mode = { "n", "v" }, desc = "Send selection to Antigravity" },
     },
+
 
 
 
